36ebb78eae02a2209db5cb2471dae4780d9b0577
riccardosantarossa <rikysanta03@gmail.com>
2022-02-15 09:44:17 +0100

    Un po di COPY PASTE

--- mylistapp/lib/main.dart
+++ mylistapp/lib/main.dart
@@ -1,89 +1,109 @@
 //SANTAROSSA RICCARDO 5BIA 13/01/2022
 
 import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'SecondScreen.dart';
+import './textdisplay.dart';
+import './button.dart';
+import 'todo.dart';
 
-class Todo {
-  final String title;
-  final String description;
+void main() => runApp(MyApp());
 
-  const Todo(this.title, this.description);
-}
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Passing Data',
-      home: TodosScreen(
-        todos: List.generate(
-          20,
-          (i) => Todo(
-            'Todo $i',
-            'Una descrizione di ciò che deve essere fatto nel Todo $i',
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-class TodosScreen extends StatelessWidget {
-  const TodosScreen({Key? key, required this.todos}) : super(key: key);
-
-  final List<Todo> todos;
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Santarossa'),
+    return MaterialApp(
+      title: 'Listapp',
+      theme: ThemeData(
+      
+        primarySwatch: Colors.brown,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,        
       ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(todos[index].title),
-            // When a user taps the ListTile, navigate to the DetailScreen.
-            // Notice that you're not only creating a DetailScreen, you're
-            // also passing the current todo through to it.
-            onTap: () {
-              showDialog(
-                  context: context,
-                  builder: (ctx) => AlertDialog(
-                        title: Text('Contenuto del todo'),
-                        content: Text(todos[index].description),
-                        actions: <Widget>[
-                          // ignore: deprecated_member_use
-                          FlatButton(
-                            autofocus: true,
-                            child: Text('OK'),
-                            onPressed: () => Navigator.of(ctx).pop(true),
-                          )
-                        ],
-                      ));
-            },
-          );
-        },
-      ),
+      home: MyHomePage(title: 'ListApp'),
+      // Define here the routes for the other app screens:
+      routes: {
+        '/secondscreen' : (ctx) => SecondScreen(),
+      } ,
     );
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // Simil overloading:
+  MyHomePage.myconstr({required this.title});
+  // final boh = MyHomePage.myconstr(title: 'Pippo',);
+  
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+
+  var _displayMessages = [
+      'Hello World',
+      'Ciao Mondo',
+      'Hallo Welt',
+      'Bonjour le Monde',
+      'Hola Mundo',
+      'Saluton mondo',
+    ];
+
+  // Lista elementi
+  final todos = List.generate(
+    20,
+    (i) => Todo(
+    'Todo $i',
+    'A description of what needs to be done for Todo $i',
+    ),
+  );
+
+  void _changeMessage() {
+    setState(() {
+      if(_index >= _displayMessages.length -1) 
+        _index = 0;
+      
+      else
+        _index += 1;
+      
+    });
+    print(_index);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final todo = ModalRoute.of(context)!.settings.arguments as Todo;
-
-    // Use the Todo to create the UI.
+    
     return Scaffold(
-      appBar: AppBar(
-        title: Text(todo.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Text(todo.description),
-      ),
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            onTap: () {
+              Navigator.of(context).pushNamed(
+              '/secondscreen',
+                arguments: {
+                'todo': todos[index],          
+              }
+            );
+      },
+    );
+  },
+        ),      
     );
   }
 }
