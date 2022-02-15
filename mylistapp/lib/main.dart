387b7a71ea616769eb1ea7ff8830122802f1e7c5
riccardosantarossa <rikysanta03@gmail.com>
2022-02-15 09:28:59 +0100

    App LISTA

---
+++ mylistapp/lib/main.dart
@@ -0,0 +1,89 @@
+//SANTAROSSA RICCARDO 5BIA 13/01/2022
+
+import 'package:flutter/material.dart';
+
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Passing Data',
+      home: TodosScreen(
+        todos: List.generate(
+          20,
+          (i) => Todo(
+            'Todo $i',
+            'Una descrizione di ciò che deve essere fatto nel Todo $i',
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+class TodosScreen extends StatelessWidget {
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
+
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Santarossa'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // When a user taps the ListTile, navigate to the DetailScreen.
+            // Notice that you're not only creating a DetailScreen, you're
+            // also passing the current todo through to it.
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                        title: Text('Contenuto del todo'),
+                        content: Text(todos[index].description),
+                        actions: <Widget>[
+                          // ignore: deprecated_member_use
+                          FlatButton(
+                            autofocus: true,
+                            child: Text('OK'),
+                            onPressed: () => Navigator.of(ctx).pop(true),
+                          )
+                        ],
+                      ));
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final todo = ModalRoute.of(context)!.settings.arguments as Todo;
+
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
