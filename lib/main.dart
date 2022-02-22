de367e525c5a863cdef747eb860ac2483949d761
riccardosantarossa <rikysanta03@gmail.com>
2022-02-22 10:02:49 +0100

    Opzione tema scuro

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       (
         primarySwatch: Colors.blue,
       ),
+      //Opzione tema scuro
       /*darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
